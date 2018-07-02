00c7905fcd84f871ee2227a124023ff891c29585
Kath <kathleen.cram@dmail.dixie.edu>
2018-07-02 10:13:34 -0600

    first commit

---
+++ client/main.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            {
+                title: 'my todo',
+                completed: false
+            },
+            {
+                title:'another item',
+                completed: true
+            }
+        ]
+    },
+
+})
