b772eb01a88d2d2d803e0521c970151a6fccad48
Kath <kathleen.cram@dmail.dixie.edu>
2018-07-03 09:45:23 -0600

    added ability to cancel and update todos

--- client/main.js
+++ client/main.js
@@ -2,7 +2,13 @@ new Vue({
     el: '#app',
     data: {
         newTodo: '',
-        todos:[]
+        editedTodo: null,
+        todos: [
+            {
+                title: 'my todo',
+                completed: false
+            }
+        ]
     },
     methods: {
         addTodo() {
@@ -14,6 +20,26 @@ new Vue({
         },
         deleteTodo(todo) {
             this.todos.splice(this.todos.indexOf(todo), 1)
+        },
+        editTodo(todo){
+            this.editedTodo = todo
+            this.cachedTitle = todo.title
+
+        },
+        saveEditedTodo(todo) {
+            todo.title = this.editedTodo.title
+            this.editedTodo = null
+        },
+        cancelEdit(todo) {
+            this.editedTodo = null
+            todo.title = this.cachedTitle
+        }
+    },
+    directives: {
+        'todo-focus'(el, binding) {
+            if (binding.value) {
+                el.focus()
+            }
         }
     }
 })
